b29cf6f3b4502c3a42271d0caaf2a54f7aa75ed0
Madmorr <97861763+Madmorr@users.noreply.github.com>
2023-09-21 21:19:10 -0400

    Image Upload/Display
    Can open a file dialog and search for and display an image on the GUI

--- GUI_Test.py
+++ GUI_Test.py
@@ -1,28 +1,38 @@
 import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
 
 # Create the main window
 window = tk.Tk()
-window.title("Basic GUI")
+window.title("AutoDrawGUI Beta 1.0")
 window.geometry("800x600")
 
 # Allow window resizing both horizontally and vertically
 window.resizable(True, True)
 
-# Create a label widget
-label = tk.Label(window, text="Hello World!")
-label.pack(pady=20)  # Add some padding around the label
+# Create a label widget to display the selected image
+imageLabel = tk.Label(window)
+imageLabel.pack(pady=30)  # Add some padding around the label
 
 
-# Create a button widget
-button = tk.Button(window, text="Click Me!")
-button.pack()
+# Function to open a file dialog for selecting an image
+def browse_image():
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png *.jpg *.jpeg")])
+    if file_path:
+        display_image(file_path)
+
 
 # Function to be called when the button is clicked
-def button_click():
-    label.config(text="Button Clicked!")
+def display_image(file_path):
+    image = Image.open(file_path)
+    image = image.resize((400, 400))  # Resize the image to fit the label
+    photo = ImageTk.PhotoImage(image=image) # Convert the image to a format that tkinter can display
+    imageLabel.config(image=photo) 
+    imageLabel.image = photo  # Keep a reference to the photo//prevents garbage collection 
 
-# Bind the button's click event to the function
-button.config(command=button_click)
+# Create a button widget to browse for an image
+browse_button = tk.Button(window, text="Browse Image", command=browse_image)
+browse_button.pack()
 
 # Start the main event loop
 window.mainloop()

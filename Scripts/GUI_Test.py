42d4c3d6b2e5d3e40290745cf21ddd6148718e56
Madmorr <97861763+Madmorr@users.noreply.github.com>
2023-10-04 20:09:21 -0400

    Added svg to g-code conversion

--- Scripts/GUI_Test.py
+++ Scripts/GUI_Test.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import os
+import subprocess
 from tkinter import filedialog
 from PIL import Image, ImageTk
 from io import BytesIO
@@ -16,16 +18,34 @@ window.geometry("800x600")
 # Allow window resizing both horizontally and vertically
 window.resizable(True, True)
 
-# Create a label widget to display the selected image
-imageLabel = tk.Label(window)
-imageLabel.pack(pady=30)  # Add some padding around the label
 
+# Define the path to the juicy-gcode executable
+path = os.getcwd()
+gCodeLit = path + r"\JuicyG-Code\juicy-gcode-1.0.0.0-Windows\juicy-gcode.exe"
+
+# global var to hold path of chosen file
+selected_file = ""
+
+def runJuicyGCode(filename):
+    gCodeArgs2 = "-o" 
+    gCodeOutputFile = "output.gcode"
+    gCodeResult = subprocess.run([gCodeLit, filename, gCodeArgs2, gCodeOutputFile], capture_output=True, text=True)
+    
+    if gCodeResult.returncode != 0:
+        print(gCodeResult.stderr)
+        return None
+    with open(gCodeOutputFile, 'r') as f:
+        gcode_content = f.read()
+    return gcode_content
 
 # Function to open a file dialog for selecting an image
 def browse_image():
+    global selected_file
     file_path = filedialog.askopenfilename(filetypes=[("SVG files", "*.svg")])
     if file_path:
+        selected_file = file_path  
         display_image(file_path)
+        generate_button.config(state=tk.NORMAL)  # Enable the "Generate G-Code" button
 
 
 # Function to be called when the button is clicked
@@ -55,10 +75,37 @@ def display_image(file_path):
     imageLabel.config(image=photo)
     imageLabel.image = photo  # Keep a reference to the photo to prevent garbage collection
 
+def generate_gcode():
+    global selected_file
+    if ".svg" in selected_file:
+        gcode = runJuicyGCode(selected_file)
+        if gcode:
+            gcodeDisplay.delete(1.0, tk.END)  # Clear the text widget
+            gcodeDisplay.insert(tk.END, gcode)  # Insert the G-code into the text widget
+        else:
+            gcodeDisplay.delete(1.0, tk.END)
+            gcodeDisplay.insert(tk.END, "Error generating G-Code.")
+    else:
+        gcodeDisplay.delete(1.0, tk.END)
+        gcodeDisplay.insert(tk.END, "Please select an SVG file first.")
+
 
 # Create a button widget to browse for an image
 browse_button = tk.Button(window, text="Browse Image", command=browse_image)
 browse_button.pack()
+
+# Create a label widget to display the selected image
+imageLabel = tk.Label(window)
+imageLabel.pack(pady=30)  # Add some padding around the label
+
+# Create a button widget to generate G-Code
+generate_button = tk.Button(window, text="Generate G-Code", command=generate_gcode, state=tk.DISABLED)
+generate_button.pack(pady=10)
+
+# Create a Text widget to display the G-code
+gcodeDisplay = tk.Text(window, width=50, height=20)
+gcodeDisplay.pack(pady=10, padx=10)
+
 
 # Start the main event loop
 window.mainloop()
